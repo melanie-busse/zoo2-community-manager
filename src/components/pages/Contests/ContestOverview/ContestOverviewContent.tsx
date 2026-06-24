452a62eb0c04516e542a956273a21a61d1d58e64
Melanie <git@melanie-busse.de>
2026-06-24 17:24:39 +0200

    fix: remove unused MobileListView component

--- src/components/pages/Contests/ContestOverview/ContestOverviewContent.tsx
+++ src/components/pages/Contests/ContestOverview/ContestOverviewContent.tsx
@@ -3,7 +3,6 @@
 import { useTranslations } from "next-intl";
 import PageHeader from "@/components/page-structure/page/PageHeader";
 import ContestDesktopTable from "@/components/pages/Contests/ContestOverview/ContestDesktopTable";
-import MobileListView from "@/components/elements/MobileListView/MobileListView";
 import { Contest } from "@/types/contest";
 import ContestMobileCard from "@/components/pages/Contests/ContestOverview/ContestMobileCard";
 import EmptyState from "@/components/elements/EmptyState/EmptyState";
@@ -24,32 +23,32 @@ export default function ContestOverviewContent({
   const router = useRouter();
   const t = useTranslations();
 
-  return (
-    <>
-      <PageHeader text={t("Contest.contestOverview.overview_title")} />
-
-      {contests.length > 0 ? (
-        <>
-          <ContestDesktopTable contests={contests} onEdit={handleEdit} onDelete={handleDelete} />
-
-          <MobileListView
-            currentItems={contests}
-            onEdit={handleEdit.toString}
-            onDelete={handleDelete.toString}
-            renderCardAction={(contest, actions) => (
-              <ContestMobileCard
-                key={contest.id}
-                contest={contest}
-                onClick={() => router.push(`/contests/${contest.id}`)}
-                onEdit={actions.onEdit.toString}
-                onDelete={actions.onDelete.toString}
-              />
-            )}
-          />
-        </>
-      ) : (
-        <EmptyState object="contests" onResetAction={() => router.refresh()} />
-      )}
-    </>
-  );
+  // return (
+  //   <>
+  //     <PageHeader text={t("Contest.contestOverview.overview_title")} />
+  //
+  //     {contests.length > 0 ? (
+  //       <>
+  //         <ContestDesktopTable contests={contests} onEdit={handleEdit} onDelete={handleDelete} />
+  //
+  //         <ContestMobileCard
+  //           currentItems={contests}
+  //           onEdit={handleEdit.toString}
+  //           onDelete={handleDelete.toString}
+  //           renderCardAction={(contest, actions) => (
+  //             <ContestMobileCard
+  //               key={contest.id}
+  //               contest={contest}
+  //               onClick={() => router.push(`/contests/${contest.id}`)}
+  //               onEdit={actions.onEdit.toString}
+  //               onDelete={actions.onDelete.toString}
+  //             />
+  //           )}
+  //         />
+  //       </>
+  //     ) : (
+  //       <EmptyState object="contests" onResetAction={() => router.refresh()} />
+  //     )}
+  //   </>
+  // );
 }
