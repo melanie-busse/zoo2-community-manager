d211044dbae32e56edf203f17556eda633eda991
Melanie <git@melanie-busse.de>
2026-07-08 10:25:56 +0200

    fix(types): remove unused ts-expect-error in Login component

--- src/components/page-structure/Header/Login.tsx
+++ src/components/page-structure/Header/Login.tsx
@@ -16,7 +16,6 @@ export default function Login() {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const user = session?.user;
-  // @ts-expect-error - Falls die Rolle noch nicht im Standard-Typ von NextAuth ist
   const userRole = user?.role || "";
 
   useClickOutside(wrapperRef, () => setShowLogout(false));
